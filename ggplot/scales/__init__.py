306f9fe136f3445b3e8e477044f634eca157f8cc
klu@enovix.com <klu@ucdavis.edu>
2014-09-04 15:41:11 -0700

    Changed scales package __init__ to import shape scale

--- ggplot/scales/__init__.py
+++ ggplot/scales/__init__.py
@@ -8,6 +8,7 @@ from .scale_colour_gradient import scale_colour_gradient as scale_colour_gradien
 from .scale_colour_gradient import scale_colour_gradient as scale_color_gradient2
 from .scale_colour_manual import scale_colour_manual
 from .scale_colour_manual import scale_colour_manual as scale_color_manual
+from .scale_shape_manual import scale_shape_manual
 from .scale_x_continuous import scale_x_continuous
 from .scale_x_discrete import scale_x_discrete
 from .scale_x_date import scale_x_date
@@ -20,11 +21,12 @@ from .scale_log import scale_y_log as scale_y_log10
 
 __all__ = ['scale_colour_brewer', 'scale_color_brewer',
            'scale_colour_gradient', 'scale_color_gradient',
-           'scale_colour_gradient2', 'scale_color_gradient2', 
+           'scale_colour_gradient2', 'scale_color_gradient2',
            'scale_colour_manual', 'scale_color_manual',
+           'scale_shape_manual',
            'scale_x_continuous', 'scale_y_continuous',
            'scale_x_discrete', 'scale_y_discrete',
-           'scale_x_reverse','scale_y_reverse', 
+           'scale_x_reverse','scale_y_reverse',
            'scale_x_date', # scale_y_date is missing
            'scale_x_log', 'scale_y_log',
            'scale_x_log10', 'scale_y_log10']
